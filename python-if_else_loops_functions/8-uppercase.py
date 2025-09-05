9c81bb3a0ac5d0f92b8ded2b180f7a3245aa7bde
Hasanali Qadirov <heseneli536@gmail.com>
2025-09-05 14:58:04 +0400

    fix

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,8 @@ def toupper(char):
     if (97 <= ord(char) < 97 + 26):
         return chr(ord(char) - 32)
     return char
+
+
 def uppercase(str):
     for i in range(len(str)):
         print("{}".format(toupper(str[i])), end="")
