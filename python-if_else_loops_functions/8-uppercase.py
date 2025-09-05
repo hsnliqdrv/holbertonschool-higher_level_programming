49a428b71c429a76764101200436f5e2c4d97f54
Hasanali Qadirov <heseneli536@gmail.com>
2025-09-05 14:43:30 +0400

    task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if (97 <= ord(str[i]) < 97 + 26):
+            print("{}".format(chr(ord(str[i]) - 32)), end="")
+        else:
+            print("{}".format(str[i]), end="")
