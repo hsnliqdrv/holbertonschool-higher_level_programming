d0bc0205b4c846439b224adec5f0b9f6dee88378
Hasanali Qadirov <heseneli536@gmail.com>
2025-09-06 17:04:29 +0400

    task 8

---
+++ python-import_modules/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub # 3
+    if (a < b): # 4
+        c = add(a, b) # 5
+        for i in range(4, 6): # 6
+            c = add(c, i) # 7
+        return c # 8
+# 9
+    return sub(a, b) # 10
