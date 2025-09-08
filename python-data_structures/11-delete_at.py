4997d65a880928a385a052794503c0bbc7e226de
Hasanali Qadirov <heseneli536@gmail.com>
2025-09-08 15:32:14 +0400

    fix

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    return my_list[:idx] + my_list[idx + 1:]
+    if (0 <= idx < len(my_list)):
+        for i in range(idx, len(my_list) - 1):
+            my_list[i] = my_list[i + 1]
+        del my_list[-1]
+    return my_list
