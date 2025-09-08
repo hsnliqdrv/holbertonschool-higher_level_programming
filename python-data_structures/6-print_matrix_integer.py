8d896b77e779d30411a8145cf1a97fbe5abb512f
Hasanali Qadirov <heseneli536@gmail.com>
2025-09-08 14:57:46 +0400

    task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for element in row:
+            print("{:d}".format(element), end="\n" if element == row[-1] else " ")
