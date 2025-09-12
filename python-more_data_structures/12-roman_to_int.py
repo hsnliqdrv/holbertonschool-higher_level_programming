1ee2cd1ab0119327510bfdd39263d5c5843b6c12
Hasanali Qadirov <heseneli536@gmail.com>
2025-09-12 16:18:07 +0400

    fix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-d = {'I':1,'V':5,'X':10,'L':50,
-    'C':100,'D':500,'M':1000}
+d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+
 def roman_to_int(roman_string):
     if (type(roman_string) is not str):
         return 0
