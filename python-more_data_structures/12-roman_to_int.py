44a10beb2684b1f98097dccefd78db4297e6d693
Hasanali Qadirov <heseneli536@gmail.com>
2025-09-12 16:16:27 +0400

    task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+d = {'I':1,'V':5,'X':10,'L':50,
+    'C':100,'D':500,'M':1000}
+def roman_to_int(roman_string):
+    if (type(roman_string) is not str):
+        return 0
+    s = 0
+    for i in range(len(roman_string)):
+        s += d[roman_string[i]]
+        if (i > 0 and d[roman_string[i - 1]] < d[roman_string[i]]):
+            s -= 2 * d[roman_string[i - 1]]
+    return s
