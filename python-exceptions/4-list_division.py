783408cae8f027fdf01c81d76fe596048d257506
hsnliqdrv <heseneli536@gmail.com>
2025-09-13 15:43:15 +0400

    fix

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -12,6 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
                 print("wrong type")
             if isinstance(e, IndexError):
                 print("out of range")
+                break
         finally:
             l[i] = c
     return l
