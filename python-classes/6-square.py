e79104245ba06d36e6c6e5240d3ebc889a3fb8e7
hsnliqdrv <heseneli536@gmail.com>
2025-09-16 19:42:40 +0400

    task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -11,7 +11,7 @@ class Square:
     Attributes:
         __size (int) - size of the square
     """
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         """Initializes a square
 
         Arguments:
@@ -67,9 +67,11 @@ class Square:
         Arguments:
             value (tuple(int,int)): new value of position
         """
-        if (type(value) is not tuple or len(value) != 2 or
-            type(value[0]) is not int or type(value[1]) is not int or 
-            value[0] < 0 or value[1] < 0):
+        if (
+                type(value) is not tuple or len(value) != 2
+                or type(value[0]) is not int or type(value[1]) is not int
+                or value[0] < 0 or value[1] < 0
+        ):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
