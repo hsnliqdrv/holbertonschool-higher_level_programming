8024ae47281f19ee16d3d6f1b41e0fdbddd76d41
hsnliqdrv <heseneli536@gmail.com>
2025-09-16 18:42:13 +0400

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""1-square
+
+This module includes a square class
+"""
+
+
+class Square:
+    """Defines a square
+
+    Attributes:
+        __size (int) - size of the square
+    """
+    def __init__(self, size):
+        """Initializes a square
+
+        Arguments:
+            size (int) - size of the square
+        """
+        self.__size = size
+    pass
