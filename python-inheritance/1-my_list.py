6248f36a5b4225faec5977bec1a19003bc2f4b24
Hasanali Qadirov <heseneli536@gmail.com>
2025-09-22 17:44:41 +0400

    Task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+"""1-my_list
+
+This module provides MyList class
+"""
+
+
+class MyList(list):
+    """Inheritance of built-in list
+    """
+
+    def print_sorted(self):
+        """Prints list in ascending order
+        """
+        print(sorted(self))
