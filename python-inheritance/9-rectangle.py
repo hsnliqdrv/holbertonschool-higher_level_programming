16d3a7a205fb811c70da258786eda0f10ac0f54b
Hasanali Qadirov <heseneli536@gmail.com>
2025-09-24 17:55:16 +0400

    task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""8-rectangle
+
+This module provides a rectangle
+"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle
+
+    Attributes:
+        width (int): width of rect
+        height (int): height of rect
+    """
+
+    def __init__(self, width, height):
+        """Initializer of rectangle
+
+        Arguments:
+            width (int): width
+            height (int): height
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculates Area
+
+        Returns:
+            (int): width * height
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """String repr
+
+        Returns:
+            (str): representation
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
