6110617a4a1d26e7299f84f7bb8b0b41f6a45887
Hasanali Qadirov <heseneli536@gmail.com>
2025-09-24 17:18:21 +0400

    task 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""7-base_geometry
+"""
+
+
+class BaseGeometry:
+    """This class represents base Geometric shapes
+    """
+
+    def area(self):
+        """Calculate area
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the value
+
+        Arguments:
+            name (str): name
+            value (int): value
+        """
+        if (not isinstance(value, int)):
+            raise TypeError(f"{name} must be an integer")
+        if (value <= 0):
+            raise ValueError(f"{name} must be greater than 0")
