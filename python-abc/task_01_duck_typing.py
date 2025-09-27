97eafe85f1e2f351a3a2050c71ed583770815a79
hsnliqdrv <heseneli536@gmail.com>
2025-09-27 14:43:20 +0400

    fix

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -14,8 +14,8 @@ class Shape(ABC):
 
 class Rectangle(Shape):
     def __init__(self, width, height):
-        self.width = abs(width)
-        self.height = abs(height)
+        self.width = width
+        self.height = height
 
     def area(self):
         return self.width * self.height
