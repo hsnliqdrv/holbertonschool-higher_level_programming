06831a361049af1b5c521a8aa99419023a3305d2
Hasanali Qadirov <heseneli536@gmail.com>
2025-09-29 16:48:48 +0400

    task 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""0-read_file
+"""
+
+
+def read_file(filename=""):
+    """Reads a text file and prints content
+
+    Arguments:
+        filename (str): the path to file
+    """
+    with open(filename, "r") as f:
+        print(f.read())
