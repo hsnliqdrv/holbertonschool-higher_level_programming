2bf6c0cd6b74ac07965950b99b0ebf7c81a1dc16
Hasanali Qadirov <heseneli536@gmail.com>
2025-09-29 19:00:28 +0400

    task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""10-student
+"""
+
+
+class Student:
+    """Represents a Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initializer
+
+        Arguments:
+            first_name (str)
+            last_name (str)
+            age (int)
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Gives dictionary description
+
+        Returns: (dict)
+        """
+        d = self.__dict__
+        if (type(attrs) is list and all(type(x) is str for x in attrs):
+            return dict([item for item in d.items() if item[0] in attrs])
+        return d
