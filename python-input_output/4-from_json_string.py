f9cb0eb8cfda88bd81d1911022dec3a791e8d4f6
Hasanali Qadirov <heseneli536@gmail.com>
2025-09-29 17:11:50 +0400

    task 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""4-from_json_string
+"""
+
+
+def from_json_string(my_str):
+    """From JSON to object
+
+    Arguments:
+        my_str (str): a json string
+
+    Returns:
+        (object): object represented by string
+    """
+    return json.loads(my_str)
