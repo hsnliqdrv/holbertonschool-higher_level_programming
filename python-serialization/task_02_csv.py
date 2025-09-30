7a22b878dfae9dc5e246fab6fd0e50f1adee5ec8
hsnliqdrv <heseneli536@gmail.com>
2025-09-30 21:54:52 +0400

    fix

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -5,7 +5,7 @@ def convert_csv_to_json(filename):
     try:
         with open(filename, "r") as file:
             rows = list(csv.DictReader(file))
-            with open("data.json") as file:
+            with open("data.json", "w") as file:
                 json.dump(rows, file)
                 return True
     except Exception:
