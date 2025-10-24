b25de59b0133340bda11fe76a237a5dac4117c9f
hsnliqdrv <heseneli536@gmail.com>
2025-10-24 21:46:29 +0400

    task 13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- List all genres and displays number of shows
+SELECT tv_genres.name AS genre,
+       COUNT(sg.show_id) AS number_of_shows
+FROM tv_genres
+LEFT JOIN tv_show_genres AS sg ON tv_genres.id = sg.genre_id
+GROUP BY tv_genres.id
+HAVING number_of_shows > 0
+ORDER BY number_of_shows;
