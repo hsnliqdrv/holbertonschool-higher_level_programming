aa0affeb0f770a01b21229328577c05d8c079e04
hsnliqdrv <heseneli536@gmail.com>
2025-10-24 20:40:22 +0400

    task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,5 +1,8 @@
 -- Create database hbtn_0d_usa and table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT PRIMARY KEY AUTO_INCREMENT,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+	id INT PRIMARY KEY AUTO_INCREMENT,
 	FOREIGN KEY (state_id) REFERENCES states(id),
-	name VARCHAR(256) NOT NULL);
+	name VARCHAR(256) NOT NULL
+);
