71c4b42eba0d72706e9b61a228fdc980da6f0e04
hsnliqdrv <heseneli536@gmail.com>
2025-10-24 21:35:44 +0400

    task 12

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- List all shows with no genre
 SELECT s.title, g.genre_id FROM tv_shows AS s
 LEFT JOIN tv_show_genres AS g ON s.id = g.show_id
-g.genre_id IS NULL
+WHERE g.genre_id IS NULL
 ORDER BY s.title, g.genre_id;
