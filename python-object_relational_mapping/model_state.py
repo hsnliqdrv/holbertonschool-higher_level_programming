2806de9d7d2e8f467deb768659778831cb85c4ff
hsnliqdrv <heseneli536@gmail.com>
2025-10-25 22:53:37 +0400

    fix

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,11 +4,12 @@
 Create class for states table
 """
 from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
 class State(Base):
+    """Represents the State entity"""
     __tablename__ = "states"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
