0fa1fbd70f2f0a1fb2df28f1980ed315da11c8e7
hsnliqdrv <heseneli536@gmail.com>
2025-10-25 22:40:03 +0400

    task 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
