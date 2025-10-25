ccb8895cd4b6cd77d85781e06e0352fa62c70065
hsnliqdrv <heseneli536@gmail.com>
2025-10-26 00:57:30 +0400

    fix

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -9,11 +9,12 @@ in the MySQL database. Each city belongs to a state via a foreign key.
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
+
 class City(Base):
     """City class for MySQL cities table, inherits from Base"""
-    
+
     __tablename__ = "cities"
-    
+
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
