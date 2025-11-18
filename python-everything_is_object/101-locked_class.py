2c1d807ad5c1ca10c40ba766b828c8d97ca4c312
hsnliqdrv <heseneli536@gmail.com>
2025-11-18 23:05:02 +0400

    fix

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -7,14 +7,4 @@ This module implements LockedClass
 
 class LockedClass:
     """This class is locked"""
-    def __setattr__(self, name, value):
-        """Only allow to set attribute 'first_name'
-
-        Arguments:
-            name (string): name of attribute
-            value (object): value of attribute
-        """
-        if name != 'first_name':
-            raise AttributeError(
-                f"'LockedClass' object has no attribute '{name}'")
-        super().__setattr__(name, value)
+    __slots__ = ['first_name']
